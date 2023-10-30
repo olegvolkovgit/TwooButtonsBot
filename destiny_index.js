e14467df78558374ffa99bfdb2d2cd174054b76f
Oleh_Volkov <oleg.volkov@existek.com>
2023-10-30 19:00:59 +0200

    buttons replaced with pool

--- destiny_index.js
+++ destiny_index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import { Markup, Telegraf } from 'telegraf';
-import dialog from './answers.js';
+import { Telegraf } from 'telegraf';
+import answers, { default as dialog } from './answers.js';
 import constants from './constants.js';
 
 let messageData;
@@ -8,6 +8,12 @@ let openName;
 let internalName;
 let userId;
 let isUserBot;
+const answersToChatIds = [
+    2,// agreeChat
+    4,// refuseChat
+    99,// ignoreChat 
+    6 //startButtonPressed
+]
 const lastAgreeMessageId = 2;
 const lastRefuseMessageId = 4;
 const lastIgnoreMessageId = 0;
@@ -19,40 +25,12 @@ bot.use(Telegraf.log());
 
 bot.on("message", onMessage.bind(this));
 
-bot.action("agree", (ctx) => {
-    onAction(ctx, lastAgreeMessageId);
-});
-
-bot.action("refuse", (ctx) => {
-    onAction(ctx, lastRefuseMessageId);
-});
-
-bot.action("ignore", (ctx) => {
-    onAction(ctx, lastIgnoreMessageId);
-});
+bot.on('poll_answer', onPollAnswer);
 
 async function onMessage(ctx) {
-    // if (JSON.stringify(ctx?.update?.message?.from.id) === process.env.respKey) {
-
-    //     let user = ctx.message.text.match(/\[(.*?)\]/)[1];
-    //     let data = ctx.message.text.replace(/\[(.*?)\]/, "");
-
-    //     data && user &&
-    //         await bot.telegram.sendMessage(user, data);
-    //     console.log("message to user is been sent");
-
-    //     return
-    // }
-
     let message = ctx?.message?.contact || ctx?.message?.text || ctx?.update?.message?.text || ctx?.message?.reply_to_message?.forum_topic_created?.name;
-    let topicMessageName = ctx?.message?.reply_to_message?.forum_topic_created?.name;
 
     let MESSAGE_PATTERN = await defineUserData(ctx);
-
-    if (!message) {
-        console.log(ctx);
-        return
-    }
 
     if (message) {
         switch (message) {
@@ -62,85 +40,38 @@ async function onMessage(ctx) {
             default:
                 break
         }
-
-        await setButtons(ctx);
     }
 
-    if (topicMessageName) {
-        switch (topicMessageName) {
-            case constants.START_TOPIC:
-                await ctx.telegram.sendMessage(
-                    process.env.postBox,
-                    MESSAGE_PATTERN,
-                    {
-                        message_thread_id: lastStartButtonMessageId
-                    }
-                );
-                break
-            case constants.AGREE_TOPIC:
-                await ctx.telegram.sendMessage(
-                    process.env.postBox,
-                    MESSAGE_PATTERN,
-                    {
-                        message_thread_id: lastAgreeMessageId
-                    }
-                );
-                break
-            case constants.REFUSE_TOPIC:
-                await ctx.telegram.sendMessage(
-                    process.env.postBox,
-                    MESSAGE_PATTERN,
-                    {
-                        message_thread_id: lastRefuseMessageId
-                    }
-                );
-                break
-            case constants.IGNORE_TOPIC:
-                await ctx.telegram.sendMessage(
-                    process.env.postBox,
-                    MESSAGE_PATTERN,
-                    {
-                        message_thread_id: lastIgnoreMessageId
-                    }
-                );
-                break
-            default:
-                break
-        }
-    }
-
-    !message && await setButtons(ctx);
+    await setPoll(ctx);
 }
 
-async function setButtons(ctx) {
-    await ctx.reply(dialog.askForChoice, Markup.inlineKeyboard([
-        [Markup.button.callback(dialog.agree, "agree")],
-        [Markup.button.callback(dialog.refuse, "refuse")],
-        [Markup.button.callback(dialog.ignore, "ignore")],
-    ]));
+async function setPoll(ctx) {
+    await ctx.replyWithQuiz(dialog.askForChoice,
+        [answers.agree, answers.refuse, answers.ignore],
+        { correct_option_id: 0, is_anonymous: false }
+    )
 }
 
-async function onAction(ctx, parameter) {
+async function onPollAnswer(ctx) {
     const MESSAGE_PATTERN = await defineUserData(ctx);
     await ctx.telegram.sendMessage(
         process.env.postBox,
         MESSAGE_PATTERN,
         {
-            message_thread_id: parameter,
+            message_thread_id: answersToChatIds[ctx.update.poll_answer.option_ids[0]],
         }
     );
-
-    ctx.reply(dialog.response);
+    await ctx.telegram.sendMessage(ctx.update.poll_answer.user.id, dialog.response);
 }
 
 async function defineUserData(ctx) {
     messageData = await getUserName(ctx);
     openName = messageData.openName;
     internalName = messageData.internalId;
-    userId = (ctx?.update?.message?.from.id || ctx?.update?.callback_query?.from?.id);
-    isUserBot = ctx?.update?.message?.from?.is_bot?.toString() || ctx?.update?.callback_query?.from?.is_bot?.toString();
+    userId = (ctx?.update?.message?.from.id || ctx?.update?.callback_query?.from?.id || ctx.update.poll_answer.user.id);
+    isUserBot = (ctx?.update?.message?.from?.is_bot?.toString() || ctx?.update?.callback_query?.from?.is_bot?.toString() || ctx.update.poll_answer.user.is_bot?.toString());
 
-    return "public name: { " + openName + " }\n" + "internal name: { " + internalName + " }\n" + "user id: { " + userId + " }" + "\n" + "is user bot { " + isUserBot + " }" + "\n" + " USER MESSAGE: \n ";
+    return "public name:  " + openName + " \n" + "internal name:  " + internalName + " \n" + "user id:  " + userId + " " + "\n" + "is user bot  " + isUserBot + " " + "\n" + "\n ";
 }
 
 async function getUserName(ctx) {
@@ -151,6 +82,8 @@ async function getUserName(ctx) {
         ctx?.update?.message?.sender_chat?.username ||
         ctx?.update?.callback_query?.from?.username ||
         ctx.username ||
+        ctx.update.poll_answer.user.username ||
+
         "закритий акаунт";
 
     let user_account_data = (ctx) => {
@@ -160,7 +93,8 @@ async function getUserName(ctx) {
             ctx?.message?.chat?.first_name ||
             ctx?.update?.message?.chat?.first_name ||
             ctx?.update?.message?.sender_chat?.first_name ||
-            ctx?.first_name
+            ctx?.first_name ||
+            ctx.update.poll_answer.user.first_name;
 
         let lastName = ctx?.update?.message?.from?.last_name ||
             ctx?.update?.callback_query?.from?.last_name ||
@@ -168,7 +102,8 @@ async function getUserName(ctx) {
             ctx?.message?.chat?.last_name ||
             ctx?.update?.message?.chat?.last_name ||
             ctx?.update?.message?.sender_chat?.last_name ||
-            ctx?.last_name
+            ctx?.last_name ||
+            ctx.update.poll_answer.user.last_name
 
         return firstName + " " + lastName;
     };
